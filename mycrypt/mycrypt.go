fe4afc30ebc1d7ab2269fe478d4b1fa2458f9b37
kristianvv <112623460+kristianvv@users.noreply.github.com>
2023-04-24 15:20:13 +0200

    Update mycrypt.go

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,20 +1,17 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅabcdefghijklmnopqrstuvwxyzæøå0123456789 .,:;")
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; KSN") //ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks == -1 {
-			kryptertMelding[i] = melding[i]
-			continue
-		}
 		if indeks+chiffer >= len(alphabet) {
 			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
 		} else {
 			kryptertMelding[i] = alphabet[indeks+chiffer]
 		}
+
 	}
 	return kryptertMelding
 }
